68288885da6bab0bd56c6ff10d3ff35e85d1e4fe
inui <oirakuma@gmail.com>
2015-06-25 19:35:44 +0900

    不要なアクションを削除。

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,9 @@
 class GamesController < ApplicationController
-  # GET /games
-  # GET /games.json
   def index
     @games = Game.all
-
-    respond_to do |format|
-      format.html { render :layout => false }
-      format.json { render json: @games }
-    end
+    render :layout => false
   end
 
-  # GET /games/1
-  # GET /games/1.json
   def show
     @game = Game.find(params[:id])
     respond_to do |format|
@@ -24,50 +16,6 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @game.add_user(@user) if @game.users.size < 3
     render_json(@game)
-  end
-
-  # GET /games/new
-  # GET /games/new.json
-  def new
-    @game = Game.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @game }
-    end
-  end
-
-  # GET /games/1/edit
-  def edit
-    @game = Game.find(params[:id])
-  end
-
-  # POST /games
-  # POST /games.json
-  def create
-    @game = Game.new(params[:game])
-
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render json: @game, status: :created, location: @game }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /games/1
-  # DELETE /games/1.json
-  def destroy
-    @game = Game.find(params[:id])
-    @game.destroy
-
-    respond_to do |format|
-      format.html { redirect_to games_url }
-      format.json { head :no_content }
-    end
   end
 
   def reset
