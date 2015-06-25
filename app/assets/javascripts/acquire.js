39273da90ac43daee3a6575e3bff44d684c1c2ea
inui <oirakuma@gmail.com>
2015-06-25 19:36:24 +0900

    不要な関数を削除し、ajax周りを整備。

--- app/assets/javascripts/acquire.js
+++ app/assets/javascripts/acquire.js
@@ -5,32 +5,29 @@ var chars = ["A","B","C","D","E","F","G","H","I","J","K","L"];
 (function(global){
   global.Acquire = Acquire;
 
-  var chainMarkers = {};
-
-  function Acquire(game) {
-    this.tiles = game.tiles;
-    this.players = game.users.map(function(u){
-      return new Player(u);
-    });
-    chainMarkers = game.chain_markers;
+  function Acquire() {
   }
 
-  Acquire.prototype.getUsers = function() {
+  Acquire.prototype.ajax = function(name, type) {
     return new Promise(function(resolve){
       $.ajax({
-        url: "/games/1/users.json",
-        type: "GET",
-        success: function(users){
-          resolve(users);
+        url: "1/"+name+".json",
+        type: type,
+        success: function(o){
+          resolve(o);
         }
       });
     });
+  }
+
+  Acquire.prototype.getUsers = function() {
+    return this.ajax("users", "GET");
   }
 
   Acquire.prototype.getTiles = function() {
     return new Promise(function(resolve){
       $.ajax({
-        url: "/games/1.json",
+        url: "1.json",
         type: "GET",
         success: function(users){
           resolve(users);
@@ -39,175 +36,12 @@ var chars = ["A","B","C","D","E","F","G","H","I","J","K","L"];
     });
   }
 
-  Acquire.prototype.pushTile = function(id) {
-//    this.players[id].tiles.push(this.getTile());
-  }
-
-  Acquire.prototype.getTile = function() {
-    return this.tiles.shift();
-  }
-
-  Acquire.prototype.getShare = function(color) {
-    return 10*this.price(color);
-  }
-
-  function sortHashByValue(h) {
-    var a = [];
-    for (var p in h)
-      a.push([p, h[p]]);
-    return a.sort(function(x, y){
-      if (x[1] < y[1]) return 1;
-      return -1;
-    });
-  }
-
-  Acquire.prototype.getStockholders = function(color) {
-    var h = {};
-    console.log(color);
-    for (var i = 0; i < this.players.length; i++) {
-      console.log(this.players[i].stocks);
-      var x = this.players[i].stocks[color];
-      if (x > 0)
-        h[i] = x;
-    }
-    console.log(h);
-    return sortHashByValue(h);
-  }
-
-  Acquire.prototype.shareToStockholders = function() {
-    //株主への配当
-    var major = this.getShare(this.board.merged);
-    var minor = major/2;
-
-    var a = this.getStockholders(this.board.merged);
-    console.log("stockholders", a);
-
-    var self = this;
-    function share(id, value) {
-      self.players[id].cash += value;
-      logView.append(id, 'was shared '+value+'.');
-    }
- 
-    if (a.length == 0) {
-      ;
-    } else if (a.length == 1) {
-      this.players[a[0][0]].cash += major;
-    } else {
-      //筆頭株主が2人
-      if (a[0][1] == a[1][1]) {
-        share(a[0][0], (major+minor)/2);
-        share(a[1][0], (major+minor)/2);
-      } else {
-        //第２株主が2人
-        if (a.length >= 3 && a[1][1] == a[2][1]) {
-          share(a[0][0], major);
-          share(a[1][0], minor/2);
-          share(a[2][0], minor/2);
-        //通常
-        } else {
-          share(a[0][0], major);
-          share(a[1][0], minor);
-        }
-      }
-    }
-
-    setTimeout(function(){
-      StockTableView.render();
-    }, 0);
-  }
-
-  Acquire.prototype.merge = function() {
-    //チェーンマーカーを返す。
-    chainMarkers[this.board.merged] = false;
-    //タイルの色を更新する
-    this.board.merge();
-    setTimeout(function(){
-      tilesView.render();
-    }, 0);
-  }
-
-  Acquire.prototype.countChain = function() {
-    var count = 0;
-    this.eachChain(function(){
-      count++;
-    });
-    return count;
-  }
-
-  //ホテルチェーンになっているカラーに対して
-  Acquire.prototype.eachChain = function(callback) {
-    for (var p in chainMarkers) {
-      if (chainMarkers[p])
-        callback(p);
-    }
-    return false;
-  }
-  
-  //ホテルチェーンになっていないカラー対して
-  Acquire.prototype.eachChainMarker = function(callback) {
-    for (var p in chainMarkers) {
-      if (!chainMarkers[p])
-        callback(p);
-    }
-    return false;
-  }
-  
-  Acquire.prototype.chained = function() {
-    for (var p in chainMarkers) {
-      if (chainMarkers[p])
-        return true;
-    }
-    return false;
-  }
-
-  Acquire.prototype.purchaseStock = function(id, color) {
-    this.players[id].stocks[color] += 1;
-    this.players[id].cash -= this.price(color);
-    this.stocks[color] -= 1;
-
-    logView.append(id, "purchased "+color+".");
-  }
-
-//  Acquire.prototype.buildChain = function(id, name, color) {
-//    this.board.setColor(name, color);
-//    chainMarkers[color] = true;
-//    this.players[id].stocks[color] += 1;
-//    this.stocks[color] -= 1;
-//
-//    logView.append(id, 'has created '+color+' on '+name+'.');
-//  }
-
   Acquire.prototype.sell = function(x) {
-    return new Promise(function(resolve){
-      $.ajax({
-        url: "/games/1/sell.json",
-        type: "POST",
-        success: function(game){
-          resolve(game);
-        }
-      });
-    });
+    return this.ajax("sell", "POST");
   }
 
   Acquire.prototype.trade = function(x) {
-    return new Promise(function(resolve){
-      $.ajax({
-        url: "/games/1/trade.json",
-        type: "POST",
-        success: function(game){
-          resolve(game);
-        }
-      });
-    });
-  }
-
-  Acquire.prototype.sellAll = function() {
-    for (var i = 0; i < this.players.length; i++) {
-      for (var p in this.players[i].stocks) {
-        this.players[i].cash += this.players[i].stocks[p]*this.price(p);
-        this.players[i].stocks[p] = 0;
-      }
-    }
+    return this.ajax("trade", "POST");
   }
 
   Acquire.prototype.isGameEnd = function() {
@@ -238,7 +72,6 @@ var chars = ["A","B","C","D","E","F","G","H","I","J","K","L"];
 })(this.self);
 
 function start(game) {
-  var acquire = new Acquire(game);
   StockTableView.render(game);
   tilesView = new TilesView({model:acquire,id: "#tiles"});
   tilesView.render();
@@ -259,15 +92,5 @@ var tilesView = null;
 var logView = new LogView({el:"#log"});
 var userId = null;
 var game = null;
-
-function checkGameUsers() {
-  $.ajax({
-    url: "/games/1/connect.json",
-    success: function(game) {
-      window.game = game;
-      start(game);
-    }
-  });
-}
-
-checkGameUsers();
+var acquire = new Acquire();
+acquire.ajax("connect").then(start, "GET");
