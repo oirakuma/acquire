0bc6f766c06c2b128e904f02330b845bba87210a
inui <oirakuma@gmail.com>
2015-06-26 17:08:34 +0900

    テストコードを追加。

--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -4,18 +4,32 @@ class GameTest < ActiveSupport::TestCase
   def setup
     @game = Game.new
     @game.save
-    @game.users << User.new
-    @game.users << User.new
-    @game.start
+
+    @user = User.create
+    @game.add_user @user
+    @game.add_user User.create
     @game.save
+
+    assert_equal 2, @game.users.size
   end
 
-  def test_1
-    @game.put_tile("1A")
-    @game.put_tile("2A")
-    @game.build_chain("1A", "red")
+  def test_put_tile
+    t = @user.tiles.first
+    assert_nil @game.put_tile(t)
+  end
+
+  def test_get_price
+    @game.placed_tiles["1A"] = "red"
+    @game.placed_tiles["2A"] = "red"
     assert_equal 200, @game.get_price("red")
+    @game.placed_tiles["3A"] = "red"
+    assert_equal 300, @game.get_price("red")
+  end
+
+  def test_purchase_stock
+    @game.placed_tiles["1A"] = "red"
+    @game.placed_tiles["2A"] = "red"
     @game.purchase_stock("red")
-    assert_equal 5800, @game.users.first.cash
+    assert_equal 5800, @user.cash
   end
 end
